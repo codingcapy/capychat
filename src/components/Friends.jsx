52113247b3462fee520d516b71c50ceb7fe60a4d
Paul K <spkim0921@gmail.com>
2024-03-22 15:33:39 -0700

    26th commit
    implementing profile picture

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -7,6 +7,7 @@ description: friends component for CapyChat client
  */
 
 import { FaUserFriends } from "react-icons/fa";
+import profilePic from "/capypaul01.jpg";
 
 export default function Friends(props) {
 
@@ -15,7 +16,8 @@ export default function Friends(props) {
             <div className="flex py-5 px-1 text-xl sticky top-0 bg-slate-800"><FaUserFriends size={25} className="text-center mx-2" />Friends</div>
             <div>
                 <div className="py-5 px-1 cursor-pointer hover:bg-slate-600 transition-all ease duration-300" onClick={() => props.clickedAddFriend()} >+ Add a Friend</div>
-                {props.friends.map((friend) => <div key={friend} onClick={() => props.clickedFriend(friend)} className="py-2 px-1 cursor-pointer hover:bg-slate-600 transition-all ease duration-300">
+                {props.friends.map((friend) => <div key={friend} onClick={() => props.clickedFriend(friend)} className="flex py-2 px-1 cursor-pointer hover:bg-slate-600 transition-all ease duration-300">
+                    <img src={profilePic} className="w-[40px]  rounded-full mr-2" />
                     {friend}
                 </div>)}
             </div>
