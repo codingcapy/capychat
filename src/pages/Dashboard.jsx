ae696e471e00f811bdd51317b92188d8983ca80f
Paul K <spkim0921@gmail.com>
2024-04-04 12:18:18 -0700

    52nd commit
    implementing block and unblock user

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -271,10 +271,10 @@ export default function Dashboard() {
                 {logoutMode && <div className="absolute z-[201] py-12 px-2 md:px-10 bg-slate-800 border border-white top-[35%] left-[13%] md:left-[40%] flex flex-col">
                     <div className="py-2">Are you sure you want to logout?</div>
                     <div className="mx-auto py-2">
-                        <form onSubmit={handleLogout}>
+                        <form onSubmit={handleLogout} className="md:flex md:flex-col">
                             <button type="submit" className="hidden md:block md:pb-1 edit-btn cursor-pointer px-5 py-2 bg-slate-700 rounded-xl hover:bg-slate-600 transition-all ease duration-300">Yes</button>
-                            <button className="hidden md:block delete-btn cursor-pointer px-5 py-2 bg-red-800 rounded-xl hover:bg-red-600 transition-all ease duration-300" onClick={() => setLogoutMode(false)}>No</button>
-                            <button className="md:hidden delete-btn cursor-pointer px-5 py-2 bg-red-800 rounded-xl hover:bg-red-600 transition-all ease duration-300" onClick={() => setLogoutMode(false)}>No</button>
+                            <button className="hidden md:block delete-btn cursor-pointer px-5 py-2 md:my-2 bg-red-800 rounded-xl hover:bg-red-600 transition-all ease duration-300" onClick={() => setLogoutMode(false)}>No</button>
+                            <button className="md:hidden delete-btn cursor-pointer px-5 py-2 md:my-2 bg-red-800 rounded-xl hover:bg-red-600 transition-all ease duration-300" onClick={() => setLogoutMode(false)}>No</button>
                             <button type="submit" className="md:hidden edit-btn cursor-pointer px-5 py-2 bg-slate-700 rounded-xl hover:bg-slate-600 transition-all ease duration-300">Yes</button>
                         </form>
                     </div>
