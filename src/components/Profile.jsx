64932320d4b3bf92bcbf4f72c63a302313596bbf
Paul K <spkim0921@gmail.com>
2024-04-16 14:05:14 -0700

    62nd commit
    implementing change username

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -16,12 +16,13 @@ import profilePic from "/capypaul01.jpg";
 
 export default function Profile() {
     const { user } = useAuthStore((state) => state)
-    const [editMode, setEditMode] = useState(false)
+    const [editPasswordMode, setEditPasswordMode] = useState(false)
+    const [editUsernameMode, setEditUsernameMode] = useState(false)
     const [message, setMessage] = useState("");
     const navigate = useNavigate();
 
-    function toggleEditMode() {
-        setEditMode(!editMode)
+    function toggleEditPasswordMode() {
+        setEditPasswordMode(!editPasswordMode)
     }
 
     async function handleEditPassword(e) {
@@ -30,8 +31,21 @@ export default function Profile() {
         const userId = user.user_id
         const updatedUser = { password };
         const res = await axios.post(`${DOMAIN}/api/users/${userId}`, updatedUser);
-        toggleEditMode();
+        toggleEditPasswordMode();
         setMessage("Password updated successfully!")
+        if (res?.data.success) {
+            navigate(`/capychat/dashboard/${userId}`);
+        }
+    }
+
+    async function handleEditUsername(e) {
+        e.preventDefault()
+        const username = e.target.username.value;
+        const userId = user.user_id
+        const updatedUsername = { username };
+        const res = await axios.post(`${DOMAIN}/api/users/update/${userId}`, updatedUsername);
+        toggleEditPasswordMode();
+        setMessage("Username updated successfully!")
         if (res?.data.success) {
             navigate(`/capychat/dashboard/${userId}`);
         }
@@ -43,15 +57,21 @@ export default function Profile() {
             <img src={profilePic} className="max-w-30 md:max-w-xs rounded-full mx-auto pb-2" />
             <p className="my-1 md:my-3">Username: {user.username}</p>
             <p className="my-1 md:my-3">Member Since: {user.created_at.slice(0, 10)}</p>
-            <button className="block rounded-xl my-1 md:my-2 py-2 px-2 bg-slate-700 text-white" >Change Username</button>
-            {editMode
+            {editUsernameMode ?
+                <form onSubmit={handleEditUsername} className="flex flex-col">
+                    <input type="text" id="username" name="username" placeholder="New Username" required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                    <button type="submit" className="rounded-xl my-5 py-2 px-2 bg-slate-700 text-white" >Change Username</button>
+                    <button className="" onClick={()=>setEditUsernameMode(false)}>Cancel</button>
+                </form>
+                : <button className="block rounded-xl my-1 md:my-2 py-2 px-2 bg-slate-700 text-white" onClick={()=>setEditUsernameMode(true)}>Username password</button>}
+            {editPasswordMode
                 ? <form onSubmit={handleEditPassword} className="flex flex-col">
                     <input type="password" id="password" name="password" placeholder="New Password" required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
                     <button type="submit" className="rounded-xl my-5 py-2 px-2 bg-slate-700 text-white" >Change password</button>
-                    <button className="" onClick={toggleEditMode}>Cancel</button>
+                    <button className="" onClick={toggleEditPasswordMode}>Cancel</button>
                 </form>
                 :
-                <button className="rounded-xl my-1 md:my-2 py-2 px-2 bg-slate-700 text-white" onClick={toggleEditMode}>Change password</button>}
+                <button className="rounded-xl my-1 md:my-2 py-2 px-2 bg-slate-700 text-white" onClick={toggleEditPasswordMode}>Change password</button>}
             <p>{message}</p>
         </div>
     )
