dee59b38f48f2f787bbcfb9fbbfaf69ca177a814
Paul K <spkim0921@gmail.com>
2024-04-22 15:19:45 -0700

    68th commit
    adjusting x axis margin and padding for send message input area for mobile viewport

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -188,10 +188,10 @@ export default function Messages(props) {
                     <div className="flex">
                         <div className="flex px-1 md:px-2 py-1 rounded-xl md:w-[800px] bg-slate-700 text-white">
                             <input type="text" id="content" name="content" placeholder="write a message" value={props.inputMessage} onChange={(e) => props.setInputMessage(e.target.value)} required className="bg-slate-700 md:w-[800px] pl-2 md:outline-none" />
-                            <div className="block md:hidden"><BsEmojiSmileFill size={30} className="mx-1" /></div>
+                            <div className="block md:hidden"><BsEmojiSmileFill size={30} className="ml-1" /></div>
                             <div className="hidden md:block"><BsEmojiSmileFill size={30} className="mx-2" /></div>
                         </div>
-                        <button type="submit" className="px-1 md:px-2 mx-1 md:mx-2 rounded-3xl bg-slate-600 text-white"><LuSendHorizonal size={25} /></button>
+                        <button type="submit" className="px-1 md:px-2 md:mx-2 rounded-3xl bg-slate-600 text-white"><LuSendHorizonal size={25} /></button>
                     </div>
                 </form>
             </div>
